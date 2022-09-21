026200a5e48d0f547d29e805aee5e0504901f166
Kadir-Yildiz <105437364+Kadir-Yildiz@users.noreply.github.com>
2022-09-21 11:16:57 +0300

    IRepository

---
+++ README.md
@@ -0,0 +1,21 @@
+# BagStore
+
+## Project Structure
+
+```
+/src
+- ApplicationCore
+- Infrastructure
+- Web
+
+/tests
+- UnitTests
+```
+
+## Packages
+```
+/ApplicationCore
+Install-Package Ardalis.Specification
+
+```
+
---
+++ src/ApplicationCore/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using ApplicationCore.Entities;
+using Ardalis.Specification;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+
+        Task<List<T>> GetAllAsync(); 
+        Task<List<T>> GetAllAsync(ISpecification<T> specification); 
+    }
+}
