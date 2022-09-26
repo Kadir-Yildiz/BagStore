c84e9aee8e862449c33a60ba85270075814bdf5e
Kadir-Yildiz <105437364+Kadir-Yildiz@users.noreply.github.com>
2022-09-26 11:15:42 +0300

    ProductSpesifications

---
+++ src/ApplicationCore/Spesifications/ProductsFilterSpesification.cs
@@ -0,0 +1,19 @@
+﻿using ApplicationCore.Entities;
+using Ardalis.Specification;
+
+
+namespace ApplicationCore.Spesifications
+{
+	public class ProductsFilterSpesification : Specification<Product>
+	{
+		public ProductsFilterSpesification(int? categoryId, int? brandId)
+		{
+			if (categoryId.HasValue)
+				Query.Where(x => x.CategoryId == categoryId);
+
+			if (brandId.HasValue)
+				Query.Where(x => x.BrandId == brandId);
+
+		}
+	}
+}
--- src/Web/Services/HomeViewModelService.cs
+++ src/Web/Services/HomeViewModelService.cs
@@ -1,16 +1,24 @@
-﻿namespace Web.Services
+﻿using ApplicationCore.Spesifications;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Web.Services
 {
     public class HomeViewModelService : IHomeViewModelService
     {
         private readonly IRepository<Product> _productRepo;
+        private readonly IRepository<Category> _categoryRepo;
+        private readonly IRepository<Brand> _brandRepo;
 
-        public HomeViewModelService(IRepository<Product> productRepo)
+        public HomeViewModelService(IRepository<Product> productRepo, IRepository<Category> categoryRepo, IRepository<Brand> brandRepo)
         {
             _productRepo = productRepo;
+            _categoryRepo = categoryRepo;
+            _brandRepo = brandRepo;
         }
         public async Task<HomeViewModel> GetHomeViewModel(int? categoryId, int? brandId)
         {
-            var products = await _productRepo.GetAllAsync();
+            var specFilter = new ProductsFilterSpesification(categoryId, brandId);
+            var products = await _productRepo.GetAllAsync(specFilter);
             var vm = new HomeViewModel();
             vm.CategoryId = categoryId;
             vm.BrandId = brandId;
@@ -21,7 +29,18 @@
                 PictureUri = x.PictureUri,
                 Price = x.Price,
             }).ToList();
-
+            vm.Categories = (await _categoryRepo.GetAllAsync()).Select(x => new SelectListItem()
+            {
+                Text = x.Name,
+                Value = x.Id.ToString(),
+                Selected = categoryId == x.Id
+            }).OrderBy(x=> x.Text).ToList();
+            vm.Brands = (await _brandRepo.GetAllAsync()).Select(x => new SelectListItem()
+            {
+                Text = x.Name,
+                Value = x.Id.ToString(),
+                Selected = brandId == x.Id
+            }).OrderBy(x => x.Text).ToList();
             return vm;
         }
     }
