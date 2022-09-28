e38aa91ab8442f69b010fca1c2270f2357f242eb
Kadir-Yildiz <105437364+Kadir-Yildiz@users.noreply.github.com>
2022-09-28 10:01:54 +0300

    Basket

---
+++ src/ApplicationCore/Entities/Basket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public class Basket : BaseEntity
+    {
+        public int BuyerId { get; set; }
+        public List<BasketItem> _items  = new List<BasketItem>();
+    }
+}
