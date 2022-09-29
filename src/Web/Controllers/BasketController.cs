a59d2794799040d8f0d8cd928740e62da18ec1d0
Kadir-Yildiz <105437364+Kadir-Yildiz@users.noreply.github.com>
2022-09-29 12:26:24 +0300

    basket index

--- src/Web/Controllers/BasketController.cs
+++ src/Web/Controllers/BasketController.cs
@@ -10,6 +10,10 @@ namespace Web.Controllers
         {
             _basketViewModelService = basketViewModelService;
         }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _basketViewModelService.GetBasketViewModelAsync());
+        }
         [HttpPost]
         public async Task<IActionResult> AddItemToBasket(int productId, int quantity = 1)
         {
